1ae31b3777521deb436efc888704c1a87ef8e6cb
Hyun Gil Kim <hkim3183@gatech.edu>
2024-05-14 11:13:29 -0400

    added analytics

---
+++ components/additional.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function additional() {
+  return (
+    <div>additional</div>
+  )
+}
--- components/intro.tsx
+++ components/intro.tsx
@@ -68,7 +68,7 @@ export default function Intro() {
         <span className="font-bold">Hi, I'm Stephen.</span> I'm a{" "}
         <span className="font-bold">full-stack data scientist</span> with{" "}
         <span className="font-bold">7 years</span> of experience. I enjoy
-         <span className="italic"> doing analysis  & building products</span>. My focus is{" "}
+         <span className="italic"> doing analysis  & building products </span>in the intersection of Finance and Technology. My focus is{" "}
         <span className="underline">Python SQL and R</span>.
       </motion.h1>
 
