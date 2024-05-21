b99cb2d8988cb32b97ff632f28804c94b3c836eb
Hyun Gil Kim <hkim3183@gatech.edu>
2024-05-21 14:45:00 -0400

    unseen additional

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <About /> 
       <Projects />
       <Skills />
-      <Additional />
+      {/* <Additional /> */}
       <Experience />
       <Contact />
     </main>
