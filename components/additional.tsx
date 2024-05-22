2ba863db7ef41fcbe07397f08f9ec68901e33838
Hyun Gil Kim <hkim3183@gatech.edu>
2024-05-21 21:16:05 -0400

    yes

--- components/additional.tsx
+++ components/additional.tsx
@@ -19,16 +19,15 @@ export default function Additional() {
             id="about"
         >
             <SectionHeading>Additional Works</SectionHeading>
-                <p className='mb-3'> 
+                <p> 
                     Microsoft Autism Hackathon 2024 - used OpenAI summarization API to prototype tool to save 30+ hours for healthcare providers.
-                    <a href="https://github.com/fsi-hack4autism/patient-onboarding-copilot">link</a>
+                    <a className='mb31' href="https://github.com/fsi-hack4autism/patient-onboarding-copilot">link</a>
                 </p>
-                <p className='mb-3'> Analyzing Apple Health Data - as a fitness tracking fanatic, I use my own Apple Health data to see interesting patterns.
-                    <a href="https://medium.com/towards-data-science/towards-well-being-with-data-science-part-1-7f8035f73aa">link</a>
-                    <a href="https://medium.com/towards-data-science/towards-well-being-with-data-science-part-2-bcaf1db96064">here</a>
+                <p> Analyzing Apple Health Data - as a fitness tracking fanatic, I use my own Apple Health data to see interesting patterns.
+                    <a className='mb32' href="https://medium.com/towards-data-science/towards-well-being-with-data-science-part-1-7f8035f73aa">link</a>
                 </p>
-                <p className='mb-3'> Iceland Tourism Data Analysis - used publicly available data to see the sudden popularity of tourism in Iceland in 2019.
-                    <a href="https://towardsdatascience.com/why-is-everyone-going-to-iceland-1f99083bee1a">link</a>
+                <p > Iceland Tourism Data Analysis - used publicly available data to see the sudden popularity of tourism in Iceland in 2019.
+                    <a className='mb33' href="https://towardsdatascience.com/why-is-everyone-going-to-iceland-1f99083bee1a">link</a>
                 </p>
 
         </motion.section>  
